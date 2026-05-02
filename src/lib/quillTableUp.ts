604ff274a4902150a1a0515d46844cf3a07c3202
JevonsGuo <woainijevons@msn.com>
2026-05-02 09:56:10 +0800

    修复富文本功能

--- src/lib/quillTableUp.ts
+++ src/lib/quillTableUp.ts
@@ -1,8 +1,8 @@
-import Quill from 'quill';
-import TableUp, { defaultCustomSelect, TableSelection, TableMenuContextmenu, TableResizeLine } from 'quill-table-up';
+import { Quill } from 'react-quill-new';
+import TableUp from 'quill-table-up';
 import 'quill-table-up/index.css';
 import 'quill-table-up/table-creator.css';
 
 Quill.register({ [`modules/${TableUp.moduleName}`]: TableUp }, true);
 
-export { TableUp, defaultCustomSelect, TableSelection, TableMenuContextmenu, TableResizeLine };
+export { Quill, TableUp };
